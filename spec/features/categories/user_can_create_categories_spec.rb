622c0627925d5df099bb6438fb211de6abe3eb6b
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-08-25 13:45:22 -0600

    Adds form for creating new category

--- spec/features/categories/user_can_create_categories_spec.rb
+++ spec/features/categories/user_can_create_categories_spec.rb
@@ -1,1 +1,21 @@
 require 'rails_helper'
+
+describe 'user can create a new category' do
+  describe 'they link from the index page' do
+    describe 'they fill in the form' do
+      it 'creates the new category' do
+        visit categories_path
+
+        click_link("Create a New Category")
+
+        expect(current_path).to eq(new_category_path)
+
+        fill_in "category[title]", with: "New Title"
+        click_on("Create Category")
+        expect(page).to have_content("New Title added!")
+        expect(current_path).to eq(categories_path)
+        expect(page).to have_content("New Title")
+      end
+    end
+  end
+end
