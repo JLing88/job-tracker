e0e9c237c58fd6075c392275912a32f5b2a7e905
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-08-25 13:50:02 -0600

    Adds uniqueness to category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Category, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:title)}
+    it {should validate_uniqueness_of(:title)}
   end
 
   describe 'relationships' do
