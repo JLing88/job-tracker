6726ddf3c0158e39b98a5896916f7a4877b4e121
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-08-25 11:40:24 -0600

    Initial Commit

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -22,7 +22,8 @@ describe Job do
     context "valid attributes" do
       it "is valid with a title, level of interest, and company" do
         company = Company.new(name: "Turing")
-        job = Job.new(title: "Developer", level_of_interest: 40, city: "Denver", company: company)
+        category = Category.create!(title: "Software")
+        job = Job.new(title: "Developer", level_of_interest: 40, city: "Denver", company: company, category_id: category.id)
         expect(job).to be_valid
       end
     end
@@ -33,5 +34,7 @@ describe Job do
       job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
       expect(job).to respond_to(:company)
     end
+
+    it {should belong_to(:category)}
   end
 end
